d530fa723e49ee547f97c6e50c2d5979cd657575
fishmarch <zhang_xy@buaa.edu.cn>
2022-08-30 14:34:17 +0800

    stereo calibration using openCV

---
+++ run.sh
@@ -0,0 +1,11 @@
+./build/calibrate_stereo \
+-n 20 \
+-u calib_imgs/left/cam.yml \
+-v calib_imgs/right/cam.yml \
+-L calib_imgs/left/ \
+-R calib_imgs/right/ \
+-o cam_stereo.yml \
+-l a \
+-r b \
+-e png
+
---
+++ run_mono.sh
@@ -0,0 +1,9 @@
+./build/calibrate \
+-n 20 \
+-w 8 \
+-h 11 \
+-s 30 \
+-d calib_imgs/right/ \
+-i b \
+-e png \
+-o calib_imgs/right/cam.yml
